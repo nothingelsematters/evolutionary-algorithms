1eb4cad3a3e05af21950e91dd045eda9ae5f998e
Simon Naumov <daretoodefy@gmail.com>
2023-03-09 14:40:49 +0600

    :construction: RuggedOneMax(k = 2), n = 15, (1 + (λ, λ))

--- src/experiment/rugged_runtime/optimal_parameters.rs
+++ src/experiment/rugged_runtime/optimal_parameters.rs
@@ -21,22 +21,22 @@ async fn rugged_optimal_parameters() {
 
     let log_n: usize = 15;
     let n: usize = 1 << log_n;
-    let population_sizes: Vec<usize> = (13..log_n as u32).map(|x| 1 << x).collect();
+    let population_sizes: Vec<usize> = (5..log_n as u32).map(|x| 1 << x).collect();
 
     let function = function::RuggedOneMax::new(2, n);
 
     for population_size in population_sizes {
-        let mu_common =
-            algorithm::mu_plus_one::Common::new(population_size, 0.99, 1.0 / (n as f64));
-        run_algorithm(&runtime, mu_common, function).await;
+        // let mu_common =
+        //     algorithm::mu_plus_one::Common::new(population_size, 0.99, 1.0 / (n as f64));
+        // run_algorithm(&runtime, mu_common, function).await;
 
-        // let one_plus_lambda_lambda = algorithm::OnePlusLambdaLambda::new(
-        //     population_size,
-        //     population_size as f64 / n as f64,
-        //     1.0 / population_size as f64,
-        // );
+        let one_plus_lambda_lambda = algorithm::OnePlusLambdaLambda::new(
+            population_size,
+            population_size as f64 / n as f64,
+            1.0 / population_size as f64,
+        );
 
-        // run_algorithm(&runtime, one_plus_lambda_lambda, function).await;
+        run_algorithm(&runtime, one_plus_lambda_lambda, function).await;
     }
     println!();
 }
