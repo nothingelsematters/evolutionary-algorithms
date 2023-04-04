403543535c06ba2560851f1c7f63d77285e7a199
Simon Naumov <daretoodefy@gmail.com>
2023-04-04 16:24:34 +0600

    :sparkles: RuggedOneMax(k = 2), n = 15, inspection

--- src/algorithm/mu_plus_one/common.rs
+++ src/algorithm/mu_plus_one/common.rs
@@ -1,14 +1,16 @@
+use rand::seq::SliceRandom;
+
 use crate::{
-    algorithm::{initialize_random, Algorithm, Mutant},
+    algorithm::{crossover, initialize_random, mutate, Algorithm, Mutant},
     function::Function,
 };
-use std::fmt::Display;
+use std::{cmp::max, fmt::Display};
 
 #[derive(Debug, Copy, Clone)]
 pub struct Common {
-    mu: usize,
-    crossover_probability: f64,
-    mutation_rate: f64,
+    pub mu: usize,
+    pub crossover_probability: f64,
+    pub mutation_rate: f64,
 }
 
 impl Common {
@@ -47,14 +49,98 @@ impl Algorithm for Common {
             function,
             population,
             |_n, population| {
-                let index = population
+                let min_fitness = population.iter().map(|x| x.fitness).min().unwrap();
+                let min_indices: Vec<usize> = population
                     .iter()
                     .enumerate()
-                    .min_by_key(|(_, x)| x.fitness)
-                    .unwrap()
-                    .0;
-                population.remove(index);
+                    .filter(|(_, x)| x.fitness == min_fitness)
+                    .map(|(x, _)| x)
+                    .collect();
+                let remove_index = *min_indices.choose(&mut rand::thread_rng()).unwrap();
+
+                population.swap_remove(remove_index);
+
+                let len = population.len();
+                population.swap(remove_index, len - 1);
             },
         )
+    }
+
+    fn stopping_criterea<F: Function>(population: &[Mutant], function: &F) -> bool {
+        function.best_fitness() <= population.last().unwrap().fitness
+    }
+}
+
+impl Common {
+    pub fn launch_with_fitness_inspection<F: Function>(&self, function: &F) -> Vec<(i64, i64)> {
+        let mut population = self.initialize(function);
+
+        let mut best_fitness = population.iter().map(|x| x.fitness).max().unwrap();
+        let mut best_crossover = population
+            .iter()
+            .enumerate()
+            .flat_map(|(i, x)| {
+                population
+                    .iter()
+                    .enumerate()
+                    .filter(move |(j, _)| i != *j)
+                    .map(move |(_, y)| (x, y))
+            })
+            .map(|(x, y)| {
+                let mut clone = x.bitvec.clone();
+                clone.or(&y.bitvec);
+                function.fitness(&clone)
+            })
+            .max()
+            .unwrap();
+
+        let mut results = vec![(best_fitness, best_crossover)];
+
+        while best_fitness < function.best_fitness() {
+            let p: f64 = rand::random();
+            let x = super::get_random(&population);
+
+            let z = if p <= self.crossover_probability {
+                let y = super::get_random(&population);
+                mutate(&crossover(x, y, 0.5), self.mutation_rate)
+            } else {
+                mutate(x, self.mutation_rate)
+            };
+
+            let new_mutant = Mutant::new(z, function);
+
+            population.push(new_mutant.clone());
+
+            // breaking ties
+            let min_fitness = population.iter().map(|x| x.fitness).min().unwrap();
+            let min_indices: Vec<usize> = population
+                .iter()
+                .enumerate()
+                .filter(|(_, x)| x.fitness == min_fitness)
+                .map(|(x, _)| x)
+                .collect();
+            let remove_index = *min_indices.choose(&mut rand::thread_rng()).unwrap();
+
+            population.swap_remove(remove_index);
+
+            // updating results
+            if remove_index != population.len() {
+                best_fitness = max(best_fitness, new_mutant.fitness);
+                let new_best_crossover = population
+                    .iter()
+                    .take(population.len() - 1)
+                    .map(|x| {
+                        let mut clone = x.bitvec.clone();
+                        clone.or(&new_mutant.bitvec);
+                        function.fitness(&clone)
+                    })
+                    .max()
+                    .unwrap();
+                best_crossover = max(best_crossover, new_best_crossover);
+                results.push((best_fitness, best_crossover));
+            }
+        }
+
+        results
     }
 }
--- src/experiment/rugged_runtime/optimal_parameters.rs
+++ src/experiment/rugged_runtime/optimal_parameters.rs
@@ -1,10 +1,16 @@
-use std::fmt::Display;
+use futures::future::join_all;
+use std::{
+    fmt::{Debug, Display},
+    io::{stdout, Write},
+    sync::{Arc, Mutex},
+    time::{Duration, Instant},
+};
 use tokio::runtime::Runtime;
 
 use super::{runtime, RUNS, THREADS};
 use crate::{
-    algorithm::{self, Algorithm},
-    function::{self, Function},
+    algorithm::{self, mu_plus_one::Common, Algorithm},
+    function::{self, Function, RuggedOneMax},
 };
 
 #[tokio::test]
@@ -19,7 +25,7 @@ async fn rugged_optimal_parameters() {
 
     let runtime = runtime();
 
-    let log_n: usize = 10;
+    let log_n: usize = 15;
     let n: usize = 1 << log_n;
     let population_sizes: Vec<usize> = (1..log_n as u32).map(|x| 1 << x).collect();
 
@@ -40,6 +46,73 @@ async fn rugged_optimal_parameters() {
         // );
 
         // run_algorithm(&runtime, one_plus_lambda_lambda, function).await;
+    }
+    println!();
+}
+
+#[tokio::test]
+async fn rugged_inspection() {
+    async fn run_task<F, L, T>(
+        algorithm: Common,
+        function: F,
+        progress_counter: Arc<Mutex<(usize, Duration)>>,
+        launcher: L,
+    ) -> T
+    where
+        F: Function + Send,
+        L: Fn(&Common, &F) -> T,
+        T: Debug,
+    {
+        let now = Instant::now();
+        let result = launcher(&algorithm, &function);
+        let elapsed = now.elapsed();
+
+        let mut guard = progress_counter.lock().expect("mutex locking");
+        guard.0 += 1;
+        guard.1 += elapsed;
+
+        println!("{result:?}");
+        println!("{} ({})", guard.0 - 1, algorithm.mu);
+
+        stdout().flush().expect("stdout flush");
+
+        result
+    }
+
+    async fn run_algorithm(runtime: &Runtime, algorithm: Common, f: RuggedOneMax) {
+        let now = Instant::now();
+        println!("Running {} on {}", algorithm, f);
+
+        let progress_counter = Arc::new(Mutex::new((0, Duration::new(0, 0))));
+
+        print!("Progress: ");
+        join_all((0..8).map(|_i| {
+            runtime.spawn(run_task(algorithm, f, progress_counter.clone(), |a, f| {
+                a.launch_with_fitness_inspection(f)
+            }))
+        }))
+        .await
+        .into_iter()
+        .for_each(|x| {
+            x.expect("join");
+        });
+
+        let elapsed = now.elapsed();
+        println!("Evaluated in {elapsed:.2?}\n");
+    }
+
+    let runtime = runtime();
+
+    let log_n: usize = 15;
+    let n: usize = 1 << log_n;
+    let population_sizes: Vec<usize> = [6, 12, 9, 4, 15].into_iter().map(|x| 1 << x).collect();
+
+    let function = function::RuggedOneMax::new(2, n);
+
+    for population_size in population_sizes {
+        let mu_common =
+            algorithm::mu_plus_one::Common::new(population_size, 0.99, 1.0 / (n as f64));
+        run_algorithm(&runtime, mu_common, function).await;
     }
     println!();
 }
